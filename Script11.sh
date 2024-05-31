80c95ff8990a9f09c27b6d3ab8aa9f889ace44be
Pratidan2925 <pratikshazende99@gmail.com>
2024-05-31 19:57:06 +0530

    Deleted functions

--- Script11.sh
+++ Script11.sh
@@ -11,10 +11,4 @@ elif [ $a -gt $b ]
 then
  echo "a is greater than b"
 elif [ $a -lt $b ]
-then
- echo "a is less than b"
-else
- echo "None of the condition met"
-fi
 
-echo "I have changed this branch"
